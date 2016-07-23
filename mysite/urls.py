6f5fd233ac3bdcf23238173123c07a3f10e7c989
lauosi <lauraosinska@gmail.com>
2016-07-23 22:57:33 +0200

    Decorating

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^my-tasks/', include('task.urls'))
+    url(r'deadline/', include('task.urls'))
 ]
